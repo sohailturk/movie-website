3e17ddbd136e0e6a1b60420da4578069c50535ae
sohailturk <sohailsarwarkhan18@gmail.com>
2022-06-07 09:59:42 +0500

    added bootstrap and basic layouts and  views

--- index.js
+++ index.js
@@ -3,12 +3,14 @@ const express = require("express");
 const app = express();
 const mongooseConnect = require("./db/mongooseConnect");
 const filmRouter = require("./routes/filmRouter")
+const expressLayouts = require("express-ejs-layouts");
 
 
 
 app.set("view engine","ejs")
 app.use(express.static(path.join(__dirname,"public")))
 app.use(express.urlencoded({extended:true}))
+app.use(expressLayouts)
 
 const port = process.env.PORT || 3000;
 
